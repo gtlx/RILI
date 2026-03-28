5a6642d98e5a6ecd590346ccdf23a01e4af0e62f
gtlx <gtlx@debian.local>
2026-03-28 17:44:22 +0800

    v0.3.5: Update version number

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -421,7 +421,7 @@ export const Settings: React.FC = () => {
           <div className="settings-item">
             <div>
               <div className="settings-item-label">RiLi 日历记账笔记</div>
-              <div className="settings-item-desc">版本 0.3.4</div>
+              <div className="settings-item-desc">版本 0.3.5</div>
             </div>
           </div>
           <div className="settings-item">
